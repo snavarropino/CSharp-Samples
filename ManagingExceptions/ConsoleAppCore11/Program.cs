dd85b1ede0aaa62bd9fd61fd27b390f527ac36f4
Sergio Navarro Pino <sergio.navarropino@gmail.com>
2017-08-19 22:33:15 +0200

    Added exceptions throw comparing  project

---
+++ ManagingExceptions/ConsoleAppCore11/Program.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ConsoleAppCore11
+{
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            try
+            {                
+                Console.WriteLine("CatchAndThrow:");
+                CatchAndThrow();
+            }
+            catch(Exception ex)
+            {
+                ex.ToConsole();
+            }
+
+            try
+            {
+
+                Console.WriteLine("==============================================");
+                Console.WriteLine("==============================================");
+                Console.WriteLine("CatchAndThrowEx:");
+                CatchAndThrowEx();
+            }
+            catch (Exception ex)
+            {
+                ex.ToConsole();
+            }
+
+            try
+            {
+                Console.WriteLine("==============================================");
+                Console.WriteLine("==============================================");
+                Console.WriteLine("CatchAndThrowNewEx:");
+                CatchAndThrowNewEx();
+            }
+            catch (Exception ex)
+            {
+                ex.ToConsole();
+            }
+        }
+
+        public static void CatchAndThrow()
+        {
+            try
+            {
+                var result = FaultyCode(5);
+            }
+            catch(Exception ex)
+            {
+                throw;
+            }
+        }
+
+        public static void CatchAndThrowEx()
+        {
+            try
+            {
+                var result = FaultyCode(5);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public static void CatchAndThrowNewEx()
+        {
+            try
+            {
+                var result = FaultyCode(5);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Didide by zero error", ex);
+            }
+        }
+
+        public static int FaultyCode(int number)
+        {
+            int divisor = 0;
+            return number / divisor;
+        }
+
+        public static void ToConsole(this Exception ex)
+        {
+            Console.WriteLine(ex.StackTrace);
+            if (ex.InnerException != null)
+            {
+                Console.WriteLine("Inner exception:");
+                Console.WriteLine(ex.InnerException.StackTrace);
+            }
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # CSharp-Samples
-Examples of C#
+
+Examples of C# related to posts I have published in my blog (in spanish)
+
+- Async/await: http://www.serginet.com/2016/05/async-await.html
+
+Another async/await explanation
+
+- Exceptions: http://www.serginet.com/xxxx
+
+Sample comparing throw vx throw ex vs throw new Exception
